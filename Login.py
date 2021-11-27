ac54fa3ec133086cffad1b806b5c7297133cee2b
OHMINJUNG <w2009@e-mirim.hs.kr>
2021-11-28 04:33:56 +0900

    회원가입, 로그인 완성

--- Login.py
+++ Login.py
@@ -1,62 +1,81 @@
-import json
-
 
 class SignUp:
     def __init__(self):
+        self.user = {"num": '',"pw":'', "score": []}
         self.id = ''
-        self.user = {'id':'','score':''}
-        self.file_path = "user.Json"
+        self.pw = ''
+        self.file_path = "user.txt"
 
 
     def setUser(self):
         print('--회원가입--')
 
         self.id = input('아이디를 입력하세요.')
-        if self.id == ' ':
+        if self.id == ' ' and self.id == '':
             print('아이디는 공백으로 설정할 수 없습니다.')
+            self.setUser()
+
+        self.pw = input('비밀번호를 입력하세요.')
+        if self.pw == ' ' and self.pw == '':
+            print('비밀번호는 공백으로 설정할 수 없습니다.')
             self.setUser()
 
 
     def save(self):
         self.user['id'] = self.id
+        self.user['pw'] = self.pw
+        test = {"num":self.id,"pw":self.pw, "score":[]}
         with open(self.file_path, 'a', encoding='utf-8') as f:
-            json.dump(self.user,f,indent='4',ensure_ascii=False)
-
-
+            f.write(str(test))
+            f.write('\n')
             f.close()
 
 
-    # def doubleCheck(self):
-    #     with open(self.file_path, 'r') as f:
-    #         self.user = json.load(f)
-    #
-    #     if  self.id in self.user['id'] :
-    #         print('다른 아이디를 사용하세요.')
-    #         self.setUser()
+
+    def doubleCheck(self):
+        f = open('user.txt','r',encoding='utf-8')
+        while True:
+            data = f.readline()
+            print(data)
+            if self.id in data:
+                print('다른 아이디를 사용하세요.')
+                self.setUser()
+
+            if data == '':
+                break
 
 
-class Login:
+class Login(SignUp):
     def __init__(self):
         self.id = ''
+        self.pw = ''
         self.user = ''
-        self.file_path = 'user.Json'
+        self.file_path = 'user.txt'
 
     def setId(self):
+        print('--로그인--')
         self.id = input('아이디를 입력하세요.')
-        with open(self.file_path,'r',encoding='utf-8') as f:
-            self.user = json.load(f)
-        if self.id in self.user:
-            print("로그인 성공")
-        else:
-            self.setId()
+        self.pw = input('비밀번호를 입력하세요.')
+        f = open('user.txt', 'r', encoding='utf-8')
+        while True:
+            data = f.readline()
+            print(data)
+            if self.id in data and self.pw in data:
+                print('로그인 성공')
+                break
+
+            if data == '':
+                print('다시 로그인 하세요.')
+                break
+
 
 if __name__ == '__main__':
-    signup = SignUp()
+    # signup = SignUp()
     login = Login()
-
-    signup.setUser()
+    #
+    # signup.setUser()
     # signup.doubleCheck()
-    signup.save()
+    # signup.save()
 
     login.setId()
 
